057302ffc231acebdfb6e366fd0f4f61093d75be
Alleviata <yuliya.honyaku@gmail.com>
2022-11-06 20:19:36 +0200

    moved the app from codesandbox

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  let [city, setCity] = useState(null);
-  let [temperature, setTemperature] = useState(null);
-  let [description, setDescription] = useState(null);
-  let [humidity, setHumidity] = useState(null);
-  let [wind, setWind] = useState(null);
-  let [iconLink, setIconLink] = useState(null);
+  let [city, setCity] = useState("Dublin");
+  let [temperature, setTemperature] = useState(14);
+  let [description, setDescription] = useState("Overcast clouds");
+  let [humidity, setHumidity] = useState("69");
+  let [wind, setWind] = useState("5.66");
+  let [iconLink, setIconLink] = useState(
+    `http://openweathermap.org/img/wn/04d@2x.png`
+  );
   let [visibility, setVisibility] = useState("hidden");
 
   function showWeather(response) {
@@ -36,22 +38,151 @@ function App() {
   }
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Weather Search Engine</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Type your city" onChange={updateCity} />
-        <input type="submit" value="Search" />
-      </form>
-      <div className={visibility}>
-        <ul>
-          <li>Temperature: {temperature}°C</li>
-          <li>Description: {description}</li>
-          <li>Humidity: {humidity}%</li>
-          <li>Wind: {wind} km/h</li>
-          <li>
-            <img src={iconLink} alt="weather icon" id="icon-big" />
-          </li>
-        </ul>
+      <div className="wrapper">
+        <div className="weather-app">
+          <form className="searchForm mb-4" onSubmit={handleSubmit}>
+            <div class="row">
+              <div class="col-9">
+                <input
+                  type="text"
+                  className="form-control city-input"
+                  placeholder="Type your city"
+                  onChange={updateCity}
+                />
+              </div>
+              <div class="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+
+          <div className={visibility}>
+            <div className="row">
+              <div className="col-md-4">
+                <p className="date">Date to be added later</p>
+                <h1 className="currCity">{city}</h1>
+                <p className="description">{description}</p>
+              </div>
+              <div className="col-md-8">
+                <div className="row">
+                  <div className="col-md-6">
+                    <img src={iconLink} alt="weather icon" id="icon-big" />
+                    <span className="tempWithUnits">
+                      <span className="currTemp">{temperature}</span>
+                      <span className="units">°C</span>
+                    </span>
+                  </div>
+                  <div className="col-md-6">
+                    <ul>
+                      <li>Humidity: {humidity}%</li>
+                      <li>Wind: {wind} km/h</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="weather-forecast">
+            <div className="container">
+              <div className="single-forecast">
+                <div className="forecast-date">Sun</div>
+                <img
+                  src="http://openweathermap.org/img/wn/10d@2x.png"
+                  alt="weather icon"
+                  className="icon-small"
+                />
+                <div className="forecast-temp">
+                  <span className="forecast-temp-max">15°C</span>
+                  <span className="forecast-temp-min">5°C</span>
+                </div>
+              </div>
+              <div className="single-forecast">
+                <div className="forecast-date">Mon</div>
+                <img
+                  src="http://openweathermap.org/img/wn/02d@2x.png"
+                  alt="weather icon"
+                  className="icon-small"
+                />
+                <div className="forecast-temp">
+                  <span className="forecast-temp-max">13°C</span>
+                  <span className="forecast-temp-min">7°C</span>
+                </div>
+              </div>
+              <div className="single-forecast">
+                <div className="forecast-date">Tue</div>
+                <img
+                  src="http://openweathermap.org/img/wn/03d@2x.png"
+                  alt="weather icon"
+                  className="icon-small"
+                />
+                <div className="forecast-temp">
+                  <span className="forecast-temp-max">15°C</span>
+                  <span className="forecast-temp-min">10°C</span>
+                </div>
+              </div>
+              <div className="single-forecast">
+                <div className="forecast-date">Wed</div>
+                <img
+                  src="http://openweathermap.org/img/wn/04d@2x.png"
+                  alt="weather icon"
+                  className="icon-small"
+                />
+                <div className="forecast-temp">
+                  <span className="forecast-temp-max">13°C</span>
+                  <span className="forecast-temp-min">7°C</span>
+                </div>
+              </div>
+              <div className="single-forecast">
+                <div className="forecast-date">Thu</div>
+                <img
+                  src="http://openweathermap.org/img/wn/09d@2x.png"
+                  alt="weather icon"
+                  className="icon-small"
+                />
+                <div className="forecast-temp">
+                  <span className="forecast-temp-max">11°C</span>
+                  <span className="forecast-temp-min">4°C</span>
+                </div>
+              </div>
+              <div className="single-forecast">
+                <div className="forecast-date">Fri</div>
+                <img
+                  src="http://openweathermap.org/img/wn/11d@2x.png"
+                  alt="weather icon"
+                  className="icon-small"
+                />
+                <div className="forecast-temp">
+                  <span className="forecast-temp-max">7°C</span>
+                  <span className="forecast-temp-min">0°C</span>
+                </div>
+              </div>
+              <div className="single-forecast">
+                <div className="forecast-date">Sat</div>
+                <img
+                  src="http://openweathermap.org/img/wn/13d@2x.png"
+                  alt="weather icon"
+                  className="icon-small"
+                />
+                <div className="forecast-temp">
+                  <span className="forecast-temp-max">3°C</span>
+                  <span className="forecast-temp-min">-5°C</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <footer>
+          <a href="https://github.com/Alleviata/weather-react">
+            Open-source code
+          </a>
+          <span> by Yuliia</span>
+        </footer>
       </div>
     </div>
   );
